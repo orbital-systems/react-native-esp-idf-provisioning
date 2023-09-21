90903ae85f22caf295a7fac4863816b5f13c7b25
Mateo Gianolio <gianoliomateo@gmail.com>
2023-09-21 15:53:52 +0200

    fix(ios): provisioning working

--- src/NativeEspIdfProvisioning.ts
+++ src/NativeEspIdfProvisioning.ts
@@ -1,7 +1,7 @@
 import type { TurboModule } from 'react-native';
 import { TurboModuleRegistry } from 'react-native';
 import type {
-  ESPDevice,
+  ESPDeviceInterface,
   ESPSecurity,
   ESPStatusResponse,
   ESPTransport,
@@ -13,8 +13,9 @@ export interface Spec extends TurboModule {
     devicePrefix: string,
     transport: ESPTransport,
     security: ESPSecurity
-  ): Promise<ESPDevice[]>;
+  ): Promise<ESPDeviceInterface[]>;
   stopESPDevicesSearch(): void;
+
   createESPDevice(
     deviceName: string,
     transport: ESPTransport,
@@ -22,15 +23,22 @@ export interface Spec extends TurboModule {
     proofOfPossesion?: string,
     softAPPassword?: string,
     username?: string
-  ): Promise<ESPDevice>;
-  connect(): Promise<ESPStatusResponse>;
-  sendData(path: string, data: string): Promise<string>;
-  isSessionEstablished(): boolean;
-  getProofOfPossession(): Promise<string | undefined>;
-  scanWifiList(): Promise<ESPWifiList>;
-  disconnect(): void;
-  provision(ssid: string, passphrase: string): Promise<ESPStatusResponse>;
-  initialiseSession(sessionPath: string): Promise<ESPStatusResponse>;
+  ): Promise<ESPDeviceInterface>;
+  connect(deviceName: string): Promise<ESPStatusResponse>;
+  sendData(deviceName: string, path: string, data: string): Promise<string>;
+  isSessionEstablished(deviceName: string): boolean;
+  getProofOfPossession(deviceName: string): Promise<string | undefined>;
+  scanWifiList(deviceName: string): Promise<ESPWifiList>;
+  disconnect(deviceName: string): void;
+  provision(
+    deviceName: string,
+    ssid: string,
+    passphrase: string
+  ): Promise<ESPStatusResponse>;
+  initialiseSession(
+    deviceName: string,
+    sessionPath: string
+  ): Promise<ESPStatusResponse>;
 }
 
 export default TurboModuleRegistry.getEnforcing<Spec>('EspIdfProvisioning');
