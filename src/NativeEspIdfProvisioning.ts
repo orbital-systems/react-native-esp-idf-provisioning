a85df0efb4759cbacec27d219fe870d0ba9c9a09
Mateo Gianolio <gianoliomateo@gmail.com>
2023-11-22 10:04:39 +0100

    fix(#23): move copy of types to turbomodule

--- src/NativeEspIdfProvisioning.ts
+++ src/NativeEspIdfProvisioning.ts
@@ -1,12 +1,48 @@
 import type { TurboModule } from 'react-native';
 import { TurboModuleRegistry } from 'react-native';
-import type {
-  ESPDeviceInterface,
-  ESPSecurity,
-  ESPStatusResponse,
-  ESPTransport,
-  ESPWifiList,
-} from './types';
+
+export enum ESPTransport {
+  ble = 'ble',
+  softap = 'softap',
+}
+
+export enum ESPSecurity {
+  unsecure = 0,
+  secure = 1,
+  secure2 = 2,
+}
+
+export enum ESPWifiAuthMode {
+  open = 0,
+  wep = 1,
+  wpa2Enterprise = 2,
+  wpa2Psk = 3,
+  wpaPsk = 4,
+  wpaWpa2Psk = 5,
+}
+
+export interface ESPDeviceInterface {
+  name: string;
+  security: ESPSecurity;
+  transport: ESPTransport;
+  connected?: boolean;
+  username?: string;
+  versionInfo?: { [key: string]: any }[];
+  capabilities?: string[];
+  advertisementData?: { [key: string]: any }[];
+}
+
+export interface ESPWifiList {
+  ssid: string;
+  rssi: number;
+  auth: ESPWifiAuthMode;
+  bssid?: string;
+  channel?: number;
+}
+
+export interface ESPStatusResponse {
+  status: string;
+}
 
 export interface Spec extends TurboModule {
   searchESPDevices(
