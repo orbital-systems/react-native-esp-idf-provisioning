d86aab49aa027ee1dc6093bc22c60f97dd30662b
Mateo Gianolio <gianoliomateo@gmail.com>
2024-06-05 12:06:36 +0200

    fix(react-native): upgrade to 0.74.1

--- example/babel.config.js
+++ example/babel.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const pak = require('../package.json');
 
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+  presets: ['module:@react-native/babel-preset'],
   plugins: [
     [
       'module-resolver',
@@ -13,5 +13,17 @@ module.exports = {
         },
       },
     ],
+  ],
+  overrides: [
+    {
+      plugins: [
+        [
+          '@babel/plugin-transform-private-methods',
+          {
+            loose: true,
+          },
+        ],
+      ],
+    },
   ],
 };
