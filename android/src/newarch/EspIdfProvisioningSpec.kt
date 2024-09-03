ccd50c4098d26f81663e2d2e75c71ac23ed389f1
Mateo Gianolio <gianoliomateo@gmail.com>
2024-09-03 15:33:58 +0200

    fix(android): fix newarch on android

---
+++ android/src/newarch/EspIdfProvisioningSpec.kt
@@ -0,0 +1,6 @@
+package com.espidfprovisioning
+
+import com.facebook.react.bridge.ReactApplicationContext
+
+abstract class EspIdfProvisioningSpec(context: ReactApplicationContext?) :
+    NativeEspIdfProvisioningSpec(context)
